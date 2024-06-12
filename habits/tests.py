382ff5d0ee55bcafcafd26ac86b5aaebeddb0bda
OleynikovVitaliy <Oleinikov.Vitalik84@gmail.com>
2024-06-12 10:01:48 +0300

    Попытка написать тесты

--- habits/tests.py
+++ habits/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase, APIClient
+from django.urls import reverse
+from rest_framework import status
 
-# Create your tests here.
+from habits.models import Habit
+from users.models import User
+
+
+class HabitTestCase(APITestCase):
+
+    def setUp(self):
+        """Создание Пользователя и Привычки"""
+        self.user = User.objects.create(
+            email='test@yandex.ru',
+            password='test'
+        )
+
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+        self.habit = Habit.objects.create(
+            user=self.user,
+            place="Дом",
+            time="22:00",
+            action="Сон",
+            duration=2,
+            period='every_day'
+        )
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework import status
+from users.models import User
 
-# Create your tests here.
+
+class UserTestCase(APITestCase):
+
+    def setUp(self):
+        """ Создание Пользователя """
+        self.user = User.objects.create(
+            email='Oleinikov.Vitalik@yandex.ru',
+            telegram_id=1461138162,
+            password='12345'
+        )
+
+    def test_user_register(self):
+        """ Создание Пользователя """
+        data = {"email": "test@yandex.ru", "telegram_id": 12345, "password": "test12345"}
+        response = self.client.post(reverse('users:register'), data=data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_detail(self):
+        """ Вывод информации о пользователе"""
+        response = self.client.get(reverse('users:user_detail', kwargs={'pk': self.user.pk}))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_delete(self):
+        """ Удаление Пользователя """
+        response = self.client.delete(reverse('users:user_delete', kwargs={'pk': self.user.pk}))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(User.objects.filter(id=self.user.pk).count(), 0)
